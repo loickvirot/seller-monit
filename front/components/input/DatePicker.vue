da1145fb994257c06366ce5b0a0432a4aaee7954
loickvirot <me@loickvirot.fr>
2024-10-01 21:59:01 +0200

    create InputDatePicker component

---
+++ front/components/input/DatePicker.vue
@@ -0,0 +1,21 @@
+<template>
+  <client-only>
+    <vue-tailwind-datepicker
+      i18n="fr"
+      v-model="value"
+      :formatter="formater"
+      as-single
+    />
+  </client-only>
+</template>
+
+<script setup lang="ts">
+import { ref } from "vue";
+
+const value = defineModel();
+
+const formater = ref({
+  date: "DD MMM YYYY",
+  month: "MMM",
+});
+</script>
